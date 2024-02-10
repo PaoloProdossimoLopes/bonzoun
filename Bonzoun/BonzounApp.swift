9445b19f8845229178875e70cc7cf6a9ef9dca3a
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-02-10 05:48:34 -0300

    Initial Commit

---
+++ Bonzoun/BonzounApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonzounApp.swift
+//  Bonzoun
+//
+//  Created by Paolo Prodossimo Lopes on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BonzounApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bonzoun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bonzoun
+//
+//  Created by Paolo Prodossimo Lopes on 10/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
